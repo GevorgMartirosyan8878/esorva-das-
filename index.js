6aac312390fd1f78191c09935d4dd9408946c9d1
GevorgMartirosyan8878 <gevorg.martirosyan.1998.08@gmail.com>
2022-06-17 23:05:33 +0400

    add main login for logging properties

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ console.log('saddkasndj');
 function newBranch() {
   console.log('branch develop')
 }
+
+const person = {
+  name: 'john',
+  age: 23,
+  isAdmin: false
+}
+
+
+for (const personKey in person) {
+  console.log(person[personKey]);
+}
